b4a0f91da17ae8e6f67cca6885ab9405a3f75f38
Tomas Mendoza <tm1453766@gmail.com>
2026-03-25 14:52:42 -0300

    feat: se documenta el endpoint de login + se genera el dto para errores

--- server/src/main/java/com/cms/controller/dto/auth/AuthRequestDTO.java
+++ server/src/main/java/com/cms/controller/dto/auth/AuthRequestDTO.java
@@ -1,9 +1,18 @@
 package com.cms.controller.dto.auth;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Body de login")
 public record AuthRequestDTO(
+        @Schema(example = "tm@gmail.com", description = "Email del usuario")
+        @NotNull
         String email,
+
+        @Schema(example = "123456", description = "Contraseña")
+        @NotNull
         String password
 ){
     public UsernamePasswordAuthenticationToken aModelo(){
