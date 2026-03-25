5ead7645d4f91dfafb5adb3958116f7d77d4c9cd
Nicolás Bossi <nbossi@hotmail.com>
2026-03-25 15:07:00 -0300

    Se agregó la imagen de fondo para la resolución de 1920px

--- client/src/features/login/pages/Login.tsx
+++ client/src/features/login/pages/Login.tsx
@@ -1,3 +1,4 @@
+import BackgroundWelcomeImage from '../components/background-welcome-image/BackgroundWelcomeImage';
 import LoginFormContainer from '../components/login-form-container/LoginFormContainer';
 import LogoContainer from '../components/logo-container/LogoContainer';
 import './login.css';
@@ -5,6 +6,7 @@ import './login.css';
 const Login = () => {
     return (
         <section className='login-page'>
+            <BackgroundWelcomeImage />
             <div className='login-container'>
                 <LogoContainer />
                 <LoginFormContainer />
