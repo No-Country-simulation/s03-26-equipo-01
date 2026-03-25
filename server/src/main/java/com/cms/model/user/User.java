6c70fe419eed2379117e799dcc3ac1e828ab626f
Tomas Ismael Mendoza <100981517+tomasmendoza-unq@users.noreply.github.com>
2026-03-25 11:59:11 -0300

    feature: implemento y testeo el service de usuario con los metodos findByMail + saveUser (#3)

--- server/src/main/java/com/cms/model/user/User.java
+++ server/src/main/java/com/cms/model/user/User.java
@@ -1,14 +1,22 @@
 package com.cms.model.user;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
+@NoArgsConstructor
+@SuperBuilder
 public abstract class User {
 
     private Long id;
     private String email;
     private String password;
-    private boolean enabled;
+    private String firstName;
+    private String lastName;
+    @Builder.Default
+    private boolean enabled = true;
 
     public String getRole(){
         return this.getClass().getSimpleName().toUpperCase();
