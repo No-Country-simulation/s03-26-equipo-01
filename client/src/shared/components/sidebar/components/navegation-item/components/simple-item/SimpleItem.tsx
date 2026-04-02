91ee8e125cc8b44cda84dd9c14def66b031994b7
Nicolás Bossi <nbossi@hotmail.com>
2026-04-02 12:43:35 -0300

    Se agregó una descripción opcional al icono de los items de navegación

--- client/src/shared/components/sidebar/components/navegation-item/components/simple-item/SimpleItem.tsx
+++ client/src/shared/components/sidebar/components/navegation-item/components/simple-item/SimpleItem.tsx
@@ -8,7 +8,10 @@ const SimpleItem = ({item}: SimpleItemProps) => {
             <button className = 'sidebar-simple-item_button'>
                 <div className = 'sidebar-simple-item_button_container'>
                     <figure className = 'sidebar-simple-item_icon-container'>
-                        <img src = {item.iconUrl} />
+                        <img 
+                            src = {item.iconUrl} 
+                            alt = {item.iconUrl ? 'icono para el item de navegación' : ''} 
+                        />
                     </figure>
                     <div className = 'sidebar-simple-item_button_text-container'>
                         <h4>{item.title}</h4>
