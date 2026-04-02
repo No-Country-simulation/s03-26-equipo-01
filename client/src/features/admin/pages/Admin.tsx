9b2beab95ac803f1429eba9d3f5f11657dd69994
Nicolás Bossi <nbossi@hotmail.com>
2026-04-02 14:20:55 -0300

    Se abstrayo las subrutas del administador en un archivo aparte y se desactivo el contenido de cada una al activar el sidebar en versión mobile

--- client/src/features/admin/pages/Admin.tsx
+++ client/src/features/admin/pages/Admin.tsx
@@ -1,25 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
 import SideBar from '../../../shared/components/sidebar/Sidebar';
 import itemsData from './admin-sidebar';
 import './admin.css';
-import { ADMIN_ADD_TESTIMONIAL_EMBED_PATH, ADMIN_BASE_PATH, ADMIN_DASHBOARD_PATH, ADMIN_MANAGER_PATH, ADMIN_READ_TESTIMONIAL_EMBED_PATH, ADMIN_TESTIMONIAL_PATH, ADMIN_USER_PATH } from '../../../core/routes/admin/admin';
+import AdminRoutes from '../routes/admin-routes';
+import { ADMIN_BASE_PATH } from '../../../core/routes/routes';
 
 const Admin = () => {
+    
     return (
         <main className = 'admin-page'>
             <SideBar 
                 itemsData = {itemsData} 
                 urlBase = {ADMIN_BASE_PATH} 
             />
-            
-            <Routes>
-                <Route path = {ADMIN_DASHBOARD_PATH} element={<p>dashboard</p>} />
-                <Route path = {ADMIN_TESTIMONIAL_PATH} element={<p>test</p>} />
-                <Route path = {ADMIN_MANAGER_PATH} element={<p>gestion</p>} />
-                <Route path = {ADMIN_USER_PATH} element={<p>user</p>} />
-                <Route path = {ADMIN_ADD_TESTIMONIAL_EMBED_PATH} element={<p>create embed</p>} />
-                <Route path = {ADMIN_READ_TESTIMONIAL_EMBED_PATH} element={<p>read embed</p>} />
-            </Routes>
+            <AdminRoutes />
         </main>
     )
 }
--- client/src/shared/components/sidebar/Sidebar.tsx
+++ client/src/shared/components/sidebar/Sidebar.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
 import type { SideBarProps } from './side-dar';
 import './styles/sidebar.css';
 import MenuIcon from './components/menu-icon/MenuIcon';
 import SideBarContent from './components/sidebar-content/SidebarContent';
+import { useState } from 'react';
 
-const SideBar = ({ itemsData, urlBase }: SideBarProps) => {
+const SideBar = ({ itemsData, urlBase}: SideBarProps) => {
 
     const [isActive, setIsActive] = useState(false);
-
+    
     const handleActive = () => setIsActive(!isActive);
 
     return (
-        <section className = 'home-sidebar'>
+        <section className = {isActive ? 'home-sidebar' : 'home-sidebar_disable'}>
             <MenuIcon onSubmit = {handleActive} isActive = {isActive} />
             {isActive && <SideBarContent itemsData = {itemsData} urlBase = {urlBase} />}
         </section>
