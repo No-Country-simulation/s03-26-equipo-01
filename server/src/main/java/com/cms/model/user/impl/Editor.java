390653dfdf8db9ae295e832676657ad5ca5405f4
Tomas Ismael Mendoza <100981517+tomasmendoza-unq@users.noreply.github.com>
2026-04-03 00:15:50 -0300

    feature: ahora los editores conoce a sus admin (#25)

--- server/src/main/java/com/cms/model/user/impl/Editor.java
+++ server/src/main/java/com/cms/model/user/impl/Editor.java
@@ -1,12 +1,16 @@
 package com.cms.model.user.impl;
 
 import com.cms.model.user.User;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@Data
 @NoArgsConstructor
 @SuperBuilder
 @EqualsAndHashCode(callSuper = true)
 public class Editor extends User {
+
+    private Admin createdBy;
 }
