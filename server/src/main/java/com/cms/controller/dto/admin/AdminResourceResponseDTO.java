acf06804e8d56b4437da67b490b5df1b9c4277a6
Tomas Mendoza <tm1453766@gmail.com>
2026-04-02 23:50:42 -0300

    feature: se plantea la estructura inicial del resourcedto

--- server/src/main/java/com/cms/controller/dto/admin/AdminResourceResponseDTO.java
+++ server/src/main/java/com/cms/controller/dto/admin/AdminResourceResponseDTO.java
@@ -1,9 +1,19 @@
 package com.cms.controller.dto.admin;
 
+import com.cms.controller.dto.category.CategoryResponseSimpleDTO;
+import com.cms.controller.dto.user.UserResponseSimpleDTO;
 import com.cms.model.user.impl.admin.AdminResource;
 
-public record AdminResourceResponseDTO() {
+import java.util.List;
+
+public record AdminResourceResponseDTO(
+        List<UserResponseSimpleDTO> users,
+        List<CategoryResponseSimpleDTO> category
+) {
     public static AdminResourceResponseDTO fromModel(AdminResource adminResource) {
-        return new AdminResourceResponseDTO();
+        return new AdminResourceResponseDTO(
+                adminResource.getUsers().stream().map(UserResponseSimpleDTO::fromModel).toList(),
+                adminResource.getCategories().stream().map(CategoryResponseSimpleDTO::fromModel).toList()
+        );
     }
 }
--- server/src/main/java/com/cms/model/user/impl/admin/AdminResource.java
+++ server/src/main/java/com/cms/model/user/impl/admin/AdminResource.java
@@ -1,4 +1,21 @@
 package com.cms.model.user.impl.admin;
 
+import com.cms.model.Category;
+import com.cms.model.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AdminResource {
+
+    private List<Category> categories;
+
+    private List<User> users;
 }
