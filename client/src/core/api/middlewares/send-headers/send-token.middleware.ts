4d073c546fa1c8683e81ab0689d043815b234fce
Nicolás Bossi <nbossi@hotmail.com>
2026-04-03 17:00:05 -0300

    Se creó otra funcionalidad para no validar si el token existe o no

--- client/src/core/api/middlewares/send-headers/send-token.middleware.ts
+++ client/src/core/api/middlewares/send-headers/send-token.middleware.ts
@@ -1,8 +1,9 @@
 import type { InternalAxiosRequestConfig } from "axios";
-import getToken from "../../../services/token/get-token";
+import { getTokenIs } from "../../../services/token/get-token";
 
 function sendToken(request: InternalAxiosRequestConfig): InternalAxiosRequestConfig {
-    request.headers.Authorization = getToken();
+    const isToken = getTokenIs();
+    if (isToken) request.headers.Authorization = isToken;
     return request;
 }
 
--- client/src/core/services/token/get-token.ts
+++ client/src/core/services/token/get-token.ts
@@ -2,8 +2,12 @@ import { TOKEN_KEY } from "./token-key";
 
 function getToken(): string {
     const token = localStorage.getItem(TOKEN_KEY);
-    if(!token) throw Error('No hay token de autenticación');
+    if(!token) throw new Error('No hay token de autenticación');
     return token;
 }
+
+export function getTokenIs(): string | null {
+    return localStorage.getItem(TOKEN_KEY);
+} 
 
 export default getToken;
