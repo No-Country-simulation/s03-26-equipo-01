213b3e2244106b1764cc879e91e1a58bf7d5ba74
Brian <macielbrianexe@gmail.com>
2026-04-06 00:08:10 -0300

    Refactor: Se agrega TestimonialSQLDAO para obtener testimonios con estados publicados

--- server/src/main/java/com/cms/persistence/sql/TestimonialSQLDAO.java
+++ server/src/main/java/com/cms/persistence/sql/TestimonialSQLDAO.java
@@ -1,7 +1,18 @@
 package com.cms.persistence.sql;
 
 import com.cms.model.testimonial.Testimonial;
+import com.cms.model.testimonial.enums.StateTestimonial;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 
 public interface TestimonialSQLDAO extends JpaRepository<Testimonial, Long> {
+
+    //Traemos a 5 testimonios publicados si hay más se puede usar la paginación para traer más de 5 testimonios en caso de que exista
+    @Query("SELECT t FROM Testimonial t WHERE t.state = :state ORDER BY t.createdAt DESC")
+    List<Testimonial> findTopByState(@Param("state") StateTestimonial state, Pageable pageable);
 }
