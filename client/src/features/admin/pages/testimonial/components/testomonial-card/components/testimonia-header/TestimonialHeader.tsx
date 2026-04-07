26949992ae70489a04543c9050e949a4e36d69e0
Nicolás Bossi <nbossi@hotmail.com>
2026-04-07 02:16:57 -0300

    Se agregó la información para el header de las tarjetas del testimonio

--- client/src/features/admin/pages/testimonial/components/testomonial-card/components/testimonia-header/TestimonialHeader.tsx
+++ client/src/features/admin/pages/testimonial/components/testomonial-card/components/testimonia-header/TestimonialHeader.tsx
@@ -4,8 +4,13 @@ import './testimonial.header.css';
 const TestimonialHeader = ({testimonial}: TestimonialHeaderProps) => {
     return (
         <header className = 'testimonial-admin-header'>
-            <p>{testimonial.witness}</p>
-            <p>{testimonial.category.name}</p>
+            <section className = 'testimonial-admin-header-editor'>
+                <div><p>{testimonial.witness[0].toUpperCase()}</p></div>
+                <p className = 'testimonial-admin-header_editor-name'>{testimonial.witness}</p>
+            </section>
+            <section className = 'testimonial-admin-header-category'>
+                <p>{testimonial.category.name}</p>
+            </section>
         </header>
     )
 }
