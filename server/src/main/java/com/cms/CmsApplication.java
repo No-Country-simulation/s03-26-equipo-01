266f20aba927077face74dc7db7c61d33e14a482
Tomas Mendoza <tm1453766@gmail.com>
2026-04-07 15:18:04 -0300

    se agrega a las dependencias elasticSearch

--- server/src/main/java/com/cms/CmsApplication.java
+++ server/src/main/java/com/cms/CmsApplication.java
@@ -3,8 +3,12 @@ package com.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
+@EnableElasticsearchRepositories
 public class CmsApplication {
 
 	public static void main(String[] args) {
