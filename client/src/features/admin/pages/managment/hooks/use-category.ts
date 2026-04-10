3948ab95e42747a6edb3049231354b6bbe2d687d
Nicolás Bossi <nbossi@hotmail.com>
2026-04-10 12:26:48 -0300

    Se manejó la gestión de estados en memoria para no conectarse con un servicio externo cada vez que hay un cambio

--- client/src/features/admin/pages/managment/hooks/use-category.ts
+++ client/src/features/admin/pages/managment/hooks/use-category.ts
@@ -11,29 +11,28 @@ const useCategory = () => {
 
     const {post, put, deleted, get} = useApi<Category>()
     const [categories, setCategories] = useState<Category[]>([]);
-    const [category, setCategory] = useState<Category | null>(null);
 
     useEffect(() => {
         get<Category[]>(getAllCategories)
             .then(newsCategories => setCategories(newsCategories))
             .catch(error => console.log(error))
-    }, [category]);
+    }, []);
 
     const addCategory = async (category: CategoryCreated) => {
         const newCategory = await post<CategoryCreated>(addCategoryService, category);
-        setCategory(newCategory);
+        setCategories(categories => [...categories, newCategory]);
         return newCategory;
     }
 
     const editCategory = async (category: CategoryCreated, id: number | undefined): Promise<Category> => {
         const editCategory = await put<CategoryCreated>(editCategoryService, category, id);
-        setCategory(editCategory);
+        setCategories(categories => categories.map(category => category.id === id ? editCategory : category));
         return editCategory;
     }
 
     const deleteCategory = async (id?: number): Promise<void> => {
         await deleted(deleteCategoryService, id);
-        setCategory(null);
+        setCategories(categories => categories.filter(category => category.id !== id));
     }
 
     return {categories, addCategory, editCategory, deleteCategory}
