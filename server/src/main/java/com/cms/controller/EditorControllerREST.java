fbcf00379da49e523e446ad4cc600d7bea9d4e98
Tomas Mendoza <tm1453766@gmail.com>
2026-04-11 19:33:47 -0300

    feature: se implementa la tabla para el banco de testimonios con data fake

--- server/src/main/java/com/cms/controller/EditorControllerREST.java
+++ server/src/main/java/com/cms/controller/EditorControllerREST.java
@@ -3,7 +3,9 @@ import com.cms.controller.annotations.EditorEndpoint;
 
 import com.cms.controller.dto.testimonial.TestimonialResponseDTO;
 
+import com.cms.controller.dto.testimonial.TestimonialResponseSimpleDTO;
 import com.cms.controller.dto.utils.PageResponseDTO;
+import com.cms.controller.dto.utils.table.TableResponseDTO;
 import com.cms.model.testimonial.Testimonial;
 import com.cms.services.EditorService;
 
@@ -16,6 +18,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController()
@@ -86,14 +90,20 @@ public class EditorControllerREST {
             @ApiResponse(responseCode = "403", description = "Acceso denegado",
                     content = @Content)
     })
-    public ResponseEntity<PageResponseDTO<TestimonialResponseDTO>> getTestimonialsToBank(
+    public ResponseEntity<TableResponseDTO<TestimonialResponseSimpleDTO>> getTestimonialsToBank(
             @RequestAttribute("userId") Long idEditor,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size) {
         Page<Testimonial> testimonialPage = editorService.getTestimonialsToBank(idEditor,page,size);
 
-        Page<TestimonialResponseDTO> response = testimonialPage.map(TestimonialResponseDTO::fromModel);
+        Page<TestimonialResponseSimpleDTO> testimonialDTO = testimonialPage.map(TestimonialResponseSimpleDTO::fromModel);
 
-        return ResponseEntity.ok(PageResponseDTO.from(response));
+        return ResponseEntity.ok(
+                TableResponseDTO.fromPage(
+                        List.of("Nº", "Testimonio","video", "imagen", "Tags", "Rating"),
+                        testimonialDTO,
+                        TestimonialResponseSimpleDTO::id
+                )
+        );
     }
 }
