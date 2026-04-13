4d548740a326aefd1a81ff2e49ae009b0fb365f4
Tomas Mendoza <tm1453766@gmail.com>
2026-04-13 15:18:32 -0300

    fix: se fixea la query de obtener los editores de un admin

--- client/src/shared/hooks/use-paginator.ts
+++ client/src/shared/hooks/use-paginator.ts
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
-import type { TablePaginator } from "../types/table/table";
-import useApi from "../../core/api/hooks/use-api";
+import { useEffect, useState } from 'react';
+import type { TablePaginator } from '../types/table/table';
+import useApi from '../../core/api/hooks/use-api';
 
-const usePaginator = <T>(execute: (page: number, size: number) => Promise<TablePaginator<T>>) => {
-
+const usePaginator = <T>(
+  execute: (page: number, size: number) => Promise<TablePaginator<T>>,
+) => {
   const [data, setData] = useState<TablePaginator<T>>();
   const [page, setPage] = useState(0);
   const [size] = useState(5);
@@ -11,7 +12,7 @@ const usePaginator = <T>(execute: (page: number, size: number) => Promise<TableP
 
   const fetchData = () => {
     get<TablePaginator<T>>(() => execute(page, size))
-      .then(data => setData(data))
+      .then((data) => setData(data))
       .catch(console.error);
   };
 
@@ -19,10 +20,11 @@ const usePaginator = <T>(execute: (page: number, size: number) => Promise<TableP
     fetchData();
   }, [page]);
 
-  const addRow = (row: T, id: number) => data?.rows.push({
-    id: id, 
-    data: row
-  });
+  const addRow = (row: T, id: number) =>
+    data?.rows.push({
+      id: id,
+      data: row,
+    });
 
   return { data, page, setPage, addRow, fetchData };
 };
