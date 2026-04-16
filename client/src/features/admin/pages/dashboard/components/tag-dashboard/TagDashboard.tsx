ad5a73541598f23324dfb409b058bf5df47587db
Nicolás Bossi <nbossi@hotmail.com>
2026-04-16 13:24:26 -0300

    Se agregó una animación para que la carga sea más amigable para el usuario mostrandose el cotnenido de a poco

--- client/src/features/admin/pages/dashboard/components/tag-dashboard/TagDashboard.tsx
+++ client/src/features/admin/pages/dashboard/components/tag-dashboard/TagDashboard.tsx
@@ -6,7 +6,7 @@ import './styles/tag-dashboard.css';
 const TagDashboard = ({currentTab, metrics}: TagDashboardProps) => {
     return (
         currentTab === TabValues.TAG && 
-        <section className = 'tag-dashboard-container'>
+        <section className = 'tag-dashboard-container falling-container'>
                 {metrics.map(metric =>
                     <MetricCard 
                         key = {metric.id} 
