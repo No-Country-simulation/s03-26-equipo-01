9710735ac3a254810f29456d2635e973a9c491f4
Nicolás Bossi <nbossi@hotmail.com>
2026-04-27 14:44:13 -0300

    Se refactorizó los componentes de la tarjeta de testimonios

--- client/src/features/admin/pages/testimonial/components/testomonial-card/TestimonialCard.tsx
+++ client/src/features/admin/pages/testimonial/components/testomonial-card/TestimonialCard.tsx
@@ -18,49 +18,38 @@ import PublishedModal from './components/published-modal/PublishedModal';
 import RejectModal from './components/reject-modal/RejectModal';
 
 const TestimonialCard = ({ testimonial }: TestimonialCardProps) => {
-  const { updateTestimonial, advance, deleted } =
-    useTestimonialState(testimonial);
-
-  return (
-    updateTestimonial && (
-      <TestimonialCardContent
-        testimonial = {updateTestimonial}
-        advance = {advance}
-        deleted = {deleted}
-      />
-    )
-  );
-};
-
-const TestimonialCardContent = ({
-  testimonial,
-  advance,
-  deleted,
-}: TestimonialCardContentProps) => {
-
+  
+  const { updateTestimonial, advance, deleted } = useTestimonialState(testimonial);
   const { id, isDiscart, isState, changeToDiscart, changeToPublished, changeToAproved, changeToDraft, refresh } = useChangeState();
 
-  return (
+  return (updateTestimonial && (
     <>
-      <article className='testimonial-admin-card-container falling-container'>
-        <TestimonialHeader testimonial={testimonial} />
-        <TestimonialState testimonial={testimonial} />
-        <TestimonialDescription testimonial={testimonial} />
-        <TestimonialTags tags={testimonial.tags} />
-        {testimonial.media && <MultimediaContent testimonial={testimonial} />}
-        <StateButtonContainer
-          changeStateButtons={
-            buttonsStateData({changeToDiscart, changeToPublished, changeToAproved, changeToDraft })[testimonial.state]
-          }
-          testimonial={testimonial}
-          />
-      </article>
+      <CardContent testimonial={updateTestimonial} changeToAproved={changeToAproved} changeToDiscart={changeToDiscart} changeToDraft={changeToDraft} changeToPublished={changeToPublished}/>
       {isState('Aprobado') && id && <AprobedModal onChangeState = {() => advance(id)} onClose = {refresh} /> }
       {isState('Publicado') && id && <PublishedModal onChangeState = {() => advance(id)} onClose = {refresh} /> }
       {isState('Borrador') && id && <RejectModal onChangeState = {() => advance(id)} onClose = {refresh} /> }
       {isDiscart && id && <DeleteModal onDelete = {() => deleted(id)} onClose = {refresh} /> }
     </>
+    )
   );
 };
+
+const CardContent = ({testimonial, changeToDiscart, changeToPublished, changeToAproved, changeToDraft}: TestimonialCardContentProps) => {
+
+  return (
+    <article className='testimonial-admin-card-container falling-container'>
+      <TestimonialHeader testimonial={testimonial} />
+      <TestimonialState testimonial={testimonial} />
+      <TestimonialDescription testimonial={testimonial} />
+      <TestimonialTags tags={testimonial.tags} />
+      {testimonial.media && <MultimediaContent testimonial={testimonial} />}
+      <StateButtonContainer
+        changeStateButtons={buttonsStateData({changeToDiscart, changeToPublished, changeToAproved, changeToDraft })[testimonial.state]}
+        testimonial={testimonial}
+      />
+    </article>
+  )
+}
+
 
 export default TestimonialCard;
